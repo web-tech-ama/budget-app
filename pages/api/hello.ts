a821fffb5c7180e5e271b2ba2a151aad0f7c9a13
mickaelamimba <81103599+mickaelamimba@users.noreply.github.com>
2022-09-11 01:54:40 +0200

    Update hello.ts

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,7 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
-import {useGetData} from "../../hooks/getData";
-import {useAuth} from "@/context/AuthUserContext";
+
 
 type Data = {
   name: string
