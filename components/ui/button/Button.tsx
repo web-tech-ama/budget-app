1ac22119ea83c09c7f84bcbe2bcdc758cf14ca24
drucilla.deroche <drucilladeroche@protonmail.com>
2022-09-14 14:22:00 +0200

    Create form user info, let users update & delete their settings

--- components/ui/button/Button.tsx
+++ components/ui/button/Button.tsx
@@ -1,17 +1,18 @@
 import React from 'react';
 import style from './button.module.scss'
 
-interface ButtonProps {
+interface ButtonProps extends React.InputHTMLAttributes<HTMLButtonElement>{
     classes? : string 
     text: string
     color?: string 
     round?: boolean 
     fullWidth?: boolean 
     disabled? : boolean 
-    children?: JSX.Element[] | JSX.Element 
+    children?: JSX.Element[] | JSX.Element
+    rest?: any 
 }
 
-const Button = ({classes,color,disabled,text,children}:ButtonProps) => {
+const Button = ({classes,color,disabled,text,children,...rest}:ButtonProps) => {
     const classname=({clas,styles}: { clas: string | undefined ,styles:string })=> {
        let className :string[] = [styles];
         if (clas != null) {
@@ -19,9 +20,11 @@ const Button = ({classes,color,disabled,text,children}:ButtonProps) => {
         }
         return className.join('');
     }
-
+    const backgroundColor = {
+        backgroundColor : color
+    }
     return (
-        <button type='submit' className={classname({clas: classes, styles: style.btn_reset})} disabled={disabled}>
+        <button style={backgroundColor} {...rest} type='submit' className={classname({clas: classes, styles: style.btn_reset})} disabled={disabled}>
            <span>{children}</span>
             {text}
         </button>
